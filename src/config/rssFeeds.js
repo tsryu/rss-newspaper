f3fba3d00248b654016984a98e2ba71f34ec2ec5
Taeseok Ryu <yuteta@gmail.com>
2024-03-17 20:57:48 +0900

    test

--- src/config/rssFeeds.js
+++ src/config/rssFeeds.js
@@ -2,8 +2,6 @@ const rssFeeds = {
   'ruliweb switch': 'https://bbs.ruliweb.com/nin/board/300004/rss',
   'ruliweb ps': 'https://bbs.ruliweb.com/ps/board/300001/rss',
   'ruliweb pc': 'https://bbs.ruliweb.com/pc/board/300007/rss',
-  'ruliweb user console': 'https://bbs.ruliweb.com/news/board/1001/rss',
-  'ruliweb user pc': 'https://bbs.ruliweb.com/news/board/1003/rss',
 };
 
 module.exports = rssFeeds;
